4901a9a7f3c2fc71d9b80af6d4453cf3802e80c9
asidor1 <aleksandra.sidor2.stud@pw.edu.pl>
2022-05-13 16:34:48 +0200

    yyy

---
+++ src/main/java/bdbt_project/SpringApplication/AppController.java
@@ -0,0 +1,14 @@
+package bdbt_project.SpringApplication;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+@Configuration
+public class AppController implements WebMvcConfigurer {
+    public void addViewControllers(ViewControllerRegistry registry) {
+        registry.addViewController("/index").setViewName("index");
+        registry.addViewController("/").setViewName("index");
+        registry.addViewController("/main").setViewName("main");
+        registry.addViewController("/login").setViewName("login");
+    }
+}
